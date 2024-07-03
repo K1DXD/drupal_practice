0608aa3d67b0e63fba528e63c401d9878876c44d
dat@opentechiz.com <dat@opentechiz.com>
2024-07-03 11:07:23 +0700

    setup drupal 10

---
+++ k1dxd_drupal_practice/web/sites/default/files/php/twig/6684cd2003374_menu--toolbar.html.twig_VO73TI4MxB7VOxs7sTo38_UOE/ad2hT9t-jgeg_D8hb5O0jr2hzxb5fpGio8HgCNEQi3E.php
@@ -0,0 +1,199 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/claro/templates/navigation/menu--toolbar.html.twig */
+class __TwigTemplate_816dbbf52529b9cd562b444db3370fe8 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension(SandboxExtension::class);
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520 = $this->extensions["Drupal\\tracer\\Twig\\Extension\\TraceableProfilerExtension"];
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520->enter($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "core/themes/claro/templates/navigation/menu--toolbar.html.twig"));
+
+        // line 24
+        $macros["menus"] = $this->macros["menus"] = $this;
+        // line 25
+        yield "
+";
+        // line 30
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(CoreExtension::callMacro($macros["menus"], "macro_menu_links", [($context["items"] ?? null), ($context["attributes"] ?? null), 0], 30, $context, $this->getSourceContext()));
+        yield "
+
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["_self", "items", "attributes", "menu_level"]);        
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520->leave($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof);
+
+        return; yield '';
+    }
+
+    // line 32
+    public function macro_menu_links($__items__ = null, $__attributes__ = null, $__menu_level__ = null, ...$__varargs__)
+    {
+        $macros = $this->macros;
+        $context = $this->env->mergeGlobals([
+            "items" => $__items__,
+            "attributes" => $__attributes__,
+            "menu_level" => $__menu_level__,
+            "varargs" => $__varargs__,
+        ]);
+
+        $blocks = [];
+
+        return ('' === $tmp = \Twig\Extension\CoreExtension::captureOutput((function () use (&$context, $macros, $blocks) {
+            $__internal_ad96c2d8979d8d23860453e7c5eb1520 = $this->extensions["Drupal\\tracer\\Twig\\Extension\\TraceableProfilerExtension"];
+            $__internal_ad96c2d8979d8d23860453e7c5eb1520->enter($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "macro", "menu_links"));
+
+            // line 33
+            yield "  ";
+            $macros["menus"] = $this;
+            // line 34
+            yield "  ";
+            if (($context["items"] ?? null)) {
+                // line 35
+                yield "    ";
+                if ((($context["menu_level"] ?? null) == 0)) {
+                    // line 36
+                    yield "      <ul";
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(CoreExtension::getAttribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", ["toolbar-menu", "claro-toolbar-menu"], "method", false, false, true, 36), 36, $this->source), "html", null, true);
+                    yield ">
+    ";
+                } else {
+                    // line 38
+                    yield "      <ul class=\"toolbar-menu\">
+    ";
+                }
+                // line 40
+                yield "    ";
+                $context['_parent'] = $context;
+                $context['_seq'] = CoreExtension::ensureTraversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 41
+                    yield "      ";
+                    // line 42
+                    $context["classes"] = ["menu-item", ((CoreExtension::getAttribute($this->env, $this->source,                     // line 44
+$context["item"], "is_expanded", [], "any", false, false, true, 44)) ? ("menu-item--expanded") : ("")), ((CoreExtension::getAttribute($this->env, $this->source,                     // line 45
+$context["item"], "is_collapsed", [], "any", false, false, true, 45)) ? ("menu-item--collapsed") : ("")), ((CoreExtension::getAttribute($this->env, $this->source,                     // line 46
+$context["item"], "in_active_trail", [], "any", false, false, true, 46)) ? ("menu-item--active-trail") : (""))];
+                    // line 49
+                    yield "      <li";
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(CoreExtension::getAttribute($this->env, $this->source, CoreExtension::getAttribute($this->env, $this->source, $context["item"], "attributes", [], "any", false, false, true, 49), "addClass", [($context["classes"] ?? null)], "method", false, false, true, 49), 49, $this->source), "html", null, true);
+                    yield ">
+        ";
+                    // line 50
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getLink($this->sandbox->ensureToStringAllowed(CoreExtension::getAttribute($this->env, $this->source, $context["item"], "title", [], "any", false, false, true, 50), 50, $this->source), $this->sandbox->ensureToStringAllowed(CoreExtension::getAttribute($this->env, $this->source, $context["item"], "url", [], "any", false, false, true, 50), 50, $this->source)), "html", null, true);
+                    yield "
+        ";
+                    // line 51
+                    if (CoreExtension::getAttribute($this->env, $this->source, $context["item"], "below", [], "any", false, false, true, 51)) {
+                        // line 52
+                        yield "          ";
+                        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(CoreExtension::callMacro($macros["menus"], "macro_menu_links", [CoreExtension::getAttribute($this->env, $this->source, $context["item"], "below", [], "any", false, false, true, 52), ($context["attributes"] ?? null), (($context["menu_level"] ?? null) + 1)], 52, $context, $this->getSourceContext()));
+                        yield "
+        ";
+                    }
+                    // line 54
+                    yield "      </li>
+    ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 56
+                yield "    </ul>
+  ";
+            }
+            
+            $__internal_ad96c2d8979d8d23860453e7c5eb1520->leave($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof);
+
+            return; yield '';
+        })())) ? '' : new Markup($tmp, $this->env->getCharset());
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "core/themes/claro/templates/navigation/menu--toolbar.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  132 => 56,  125 => 54,  119 => 52,  117 => 51,  113 => 50,  108 => 49,  106 => 46,  105 => 45,  104 => 44,  103 => 42,  101 => 41,  96 => 40,  92 => 38,  86 => 36,  83 => 35,  80 => 34,  77 => 33,  60 => 32,  48 => 30,  45 => 25,  43 => 24,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/claro/templates/navigation/menu--toolbar.html.twig", "/opt/drupal/web/core/themes/claro/templates/navigation/menu--toolbar.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("import" => 24, "macro" => 32, "if" => 34, "for" => 40, "set" => 42);
+        static $filters = array("escape" => 36);
+        static $functions = array("link" => 50);
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['import', 'macro', 'if', 'for', 'set'],
+                ['escape'],
+                ['link'],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}

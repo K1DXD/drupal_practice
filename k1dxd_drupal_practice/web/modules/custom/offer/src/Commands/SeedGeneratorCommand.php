820ae6211eb0411bd21db850d5233e4d46c433c5
dat@opentechiz.com <dat@opentechiz.com>
2024-07-03 15:07:41 +0700

    intinal setup

---
+++ k1dxd_drupal_practice/web/modules/custom/offer/src/Commands/SeedGeneratorCommand.php
@@ -0,0 +1,27 @@
+<?php
+namespace Drupal\offer\Commands;
+
+use Drush\Commands\DrushCommands;
+use Drupal\offer\SeedData\SeedDataGenerator;
+use Drush\Drush;
+
+/**
+ * Class SeedGeneratorCommand
+ * @package Drupal\offer\Commands
+ */
+class SeedGeneratorCommand extends DrushCommands {
+
+  /**
+   * Runs the OfferCreateSeeds command. Will create all data for the Offer platform.
+   *
+   * @command offer-create-seeds
+   * @aliases offercs
+   * @usage drush offer-create-seeds
+   * Display 'Seed data created'
+   */
+  public function offerCreateSeeds() {
+    $seed = new SeedDataGenerator();
+    $count = $seed->generate('user');
+    Drush::output()->writeln($count . ' user(s) created');
+  }
+}

5d2b888d55f201deee75eac1297f9508cbd9d3a9
dat@opentechiz.com <dat@opentechiz.com>
2024-07-04 17:27:11 +0700

    add offer view collection, add it to toolbar

---
+++ k1dxd_drupal_practice/web/modules/custom/offer/src/OfferViewsData.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\offer;
+
+use Drupal\views\EntityViewsData;
+
+class OfferViewsData extends EntityViewsData
+{
+
+  /**
+   * @return array
+   */
+  public function getViewsData() {
+    $data = parent::getViewsData();
+    return $data;
+  }
+
+}
